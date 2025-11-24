d090b454b5e4bf957967de09c2fc33b1becab11c
wongbernice <byhwong@fierysnow.com>
2025-11-24 10:58:04 -0500

    Added functions to filter_dataset.py

--- code/filter_dataset.py
+++ code/filter_dataset.py
@@ -1,72 +1,52 @@
 import os
 import shutil
 
-# Get dataset paths
-original_dataset_path = '../fruits-360'
-original_training_path = '../fruits-360/Training'
-original_testing_path = '../fruits-360/Testing'
-
-filtered_dataset_path = '../filtered-dataset'
-filtered_training_path = '../filtered-dataset/Training'
-filtered_testing_path = '../filtered-dataset/Testing'
-
-# ================================================ #
-# Filter dataset to only include selected classes
-# ================================================ #
-
-# Get all classes in original dataset
-all_training_classes = os.listdir(original_training_path)
-all_testing_classes = os.listdir(original_testing_path)
-
-# 10 selected fruit classes
-classes = ['Apple', 'Banana', 'Blueberry', 'Cherry', 'Lemon', 'Mango', 'Orange', 'Pineapple', 'Strawberry', 'Watermelon']
-
-# Filter Training Set
-for fruit in classes:
-    print(f"Filtering Training images: {fruit}")
+def filter_dataset(original_path, filtered_path, classes):
+    '''
+    Copies only selected classes from the original dataset to the filtered dataset
     
-    # Create new folder if it does not exist
-    new_fruit_path = os.path.join(filtered_training_path, fruit)
-    os.makedirs(new_fruit_path, exist_ok=True)
+    original_path: path to original dataset
+    filtered_path: path to filtered dataset
+    classes: list of selected class (fruit) names
+    '''
 
-    # Loop through all original training folders
-    for folder in os.listdir(original_training_path):
+    for fruit in classes:
+        print(f"Filtering images for {fruit}")
+        
+        # Create new class folder if it does not exist
+        new_fruit_path = os.path.join(filtered_path, fruit)
+        os.makedirs(new_fruit_path, exist_ok=True)
 
-        # If folder matches selected fruit, copy to new folder location
-        if fruit in folder:
-            src_folder = os.path.join(original_training_path, folder) # original folder path
+        # Loop through all original folders
+        for folder in os.listdir(original_path):
 
-            # Copy all images from original folder to filtered folder
-            for image in os.listdir(src_folder):
-                src_image = os.path.join(src_folder, image)
-                dst_image = os.path.join(new_fruit_path, image)
-                shutil.copy2(src_image, dst_image)
-    
-print(f"Completed Training Filtering")
+            # If folder matches selected fruit, copy to new folder location
+            if fruit in folder:
+                src_folder = os.path.join(original_path, folder) # original folder path
 
+                # Copy all images from original folder to filtered folder
+                for image in os.listdir(src_folder):
+                    src_image = os.path.join(src_folder, image)
+                    dst_image = os.path.join(new_fruit_path, image)
+                    shutil.copy2(src_image, dst_image)
+        
+        print(f"Completed filtering for {fruit}")
 
-# Filter Testing Set
-for fruit in classes:
-    print(f"Filtering Testing images: {fruit}")
+def main():
+    # Get dataset paths
+    original_training_path = '../fruits-360/Training'
+    original_testing_path = '../fruits-360/Test'
+    filtered_training_path = '../filtered-dataset/Training'
+    filtered_testing_path = '../filtered-dataset/Testing'
 
-    # Create new folder if it does not exist
-    new_fruit_path = os.path.join(filtered_testing_path, fruit)
-    os.makedirs(new_fruit_path, exist_ok=True)
+    # 10 selected fruit classes
+    classes = ['Apple', 'Banana', 'Blueberry', 'Cherry', 'Lemon', 'Mango', 'Orange', 'Pineapple', 'Strawberry', 'Watermelon']
 
-    # Loop through all original training folders
-    for folder in os.listdir(original_testing_path):
+    filter_dataset(original_training_path, filtered_training_path, classes)
+    print(f"\nCompleted filtering for training")
 
-        # If folder matches selected fruit, copy to new folder location
-        if fruit in folder:
-            src_folder = os.path.join(original_testing_path, folder) # original folder path
+    filter_dataset(original_testing_path, filtered_testing_path, classes)
+    print(f"\nCompleted filtering for testing")
 
-            # Copy all images from original folder to filtered folder
-            for image in os.listdir(src_folder):
-                src_image = os.path.join(src_folder, image)
-                dst_image = os.path.join(new_fruit_path, image)
-                shutil.copy2(src_image, dst_image)
-    
-    print(f"Completed Testing Filtering")
-    
-
-    
+if __name__ == "__main__":
+    main()
